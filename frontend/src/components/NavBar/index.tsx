2d34c1a007b8f889e341862d6639430ddbf1ac04
eliseunetto <eliseunetto@hotmail.com>
2021-05-09 16:28:36 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <h2>
-            eliseu<span className="logo">netto</span>
-          </h2>
+          <Link style={{ textDecoration: "none" }} to="/">
+            <h2>
+              eliseu<span className="logo">netto</span>
+            </h2>
+          </Link>
         </nav>
       </div>
     </div>
