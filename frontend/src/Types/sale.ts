a67ed45627216ae92465ac9d212720e131b3123a
eliseunetto <eliseunetto@hotmail.com>
2021-05-09 21:19:40 -0300

    DataTable integration

--- frontend/src/Types/sale.ts
+++ frontend/src/Types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller";
+
+export type sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: sale[];
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  size?: number;
+  number: number;
+  numberOfElements?: number;
+  first: boolean;
+  empty?: boolean;
+};
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
